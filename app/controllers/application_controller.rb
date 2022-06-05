bc56de21a44d37ca9d5f6181fd47ba05426e9f47
ksmxxxxxx <kankichi69@gmail.com>
2022-06-05 18:16:34 +0900

    WIP Try add toggle to display of language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+  around_action :switch_locale
+
+  def set_locale
+    I18n.locale = locale
+  end
+
+  def locale
+    @locale ||= params[:locale] || I18n.default_locale
+  end
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options(options={})
+    options.merge(locale: locale)
+  end
 end

dc8f29f1a35c4991f692f4b11797b96081433595
ksmxxxxxx <kankichi69@gmail.com>
2022-06-09 21:57:56 +0900

    Fix default url option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :set_locale
   around_action :switch_locale
-
-  def set_locale
-    I18n.locale = locale
-  end
-
-  def locale
-    @locale ||= params[:locale] || I18n.default_locale
-  end
 
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
   end
 
-  def default_url_options(options={})
-    options.merge(locale: locale)
+  def default_url_options
+    { locale: I18n.locale }
   end
 end

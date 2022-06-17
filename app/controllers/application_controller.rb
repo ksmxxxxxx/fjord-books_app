aced3a27adfdf8809c00828e2822b4dd84313017
ksmxxxxxx <kankichi69@gmail.com>
2022-06-17 17:13:44 +0900

    User can sign out everywhere

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resources)
+    books_path
+  end
+
+  def after_sign_out_path_for(resources)
+    new_user_session_path
+  end
 end

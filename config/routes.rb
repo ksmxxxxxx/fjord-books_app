625605e416cf1df298e857d3a3816e7594adb569
ksmxxxxxx <kankichi69@gmail.com>
2022-06-19 14:59:43 +0900

    When after update account info, transit to user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {resistrations: 'users/resistrations'}
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources  :books
   resources  :users, only: [:index, :show]
   root to: "books#index"

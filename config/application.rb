a37e948f743a759f30b5039ebacfd1c2e7de277c
ksmxxxxxx <kankichi69@gmail.com>
2022-06-21 21:36:29 +0900

    Set translation to Japanese and genarate view file with slim

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module BooksApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.default_locale = :ja
+    config.generators.template_engine = :slim
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

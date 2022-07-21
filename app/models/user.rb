764eac470f648d11a242480092a18b7f69ac5455
ksmxxxxxx <kankichi69@gmail.com>
2022-07-21 17:11:57 +0900

    Associate following model and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :active_relationships, class_name: 'Following',
+                                  foreign_key: 'follower_id',
+                                  inverse_of: 'follower',
+                                  dependent: :destroy
+
+  has_many :followees,
+           through: :active_relationships,
+           source: :follower
+
+  has_many :passive_relatioships, class_name: 'Following',
+                                  foreign_key: 'followed_id',
+                                  inverse_of: 'followed',
+                                  dependent: :destroy
+
+  has_many :followers,
+           through: :passive_relatioships,
+           source: :followed
 end

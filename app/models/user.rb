fc8a77db80ff5560f02e54c0557d3ea2e9dc6703
ksmxxxxxx <kankichi69@gmail.com>
2022-07-22 15:59:57 +0900

    Able to seeing follower count and followed count

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,20 +7,20 @@ class User < ApplicationRecord
   has_one_attached :avatar
 
   has_many :active_relationships, class_name: 'Following',
-                                  foreign_key: 'follower_id',
-                                  inverse_of: 'follower',
-                                  dependent: :destroy
-
-  has_many :followings,
-           through: :active_relationships,
-           source: :follower
-
-  has_many :passive_relatioships, class_name: 'Following',
                                   foreign_key: 'followed_id',
                                   inverse_of: 'followed',
                                   dependent: :destroy
 
+  has_many :followings,
+           through: :active_relationships,
+           source: :followed
+
+  has_many :passive_relatioships, class_name: 'Following',
+                                  foreign_key: 'follower_id',
+                                  inverse_of: 'follower',
+                                  dependent: :destroy
+
   has_many :followers,
            through: :passive_relatioships,
-           source: :followed
+           source: :follower
 end

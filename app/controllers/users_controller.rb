ef90360c0812928ded83517c27b1fbf18f41f32a
ksmxxxxxx <kankichi69@gmail.com>
2022-07-24 14:22:24 +0900

    User can seeing followed users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show followings]
   def index
     @users = User.with_attached_avatar.order(:id).page(params[:page])
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  def show; end
 
   def followings
     @followings = @user.followings
@@ -15,5 +14,11 @@ class UsersController < ApplicationController
 
   def followers
     @followers = @user.followers
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end

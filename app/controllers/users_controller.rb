dc96ae5bc2ab9fa1c578faeb25f48935109d483f
ksmxxxxxx <kankichi69@gmail.com>
2022-07-24 15:09:21 +0900

    User can seeing who they are following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show followings]
+  before_action :set_user, only: %i[show followings followers]
   def index
     @users = User.with_attached_avatar.order(:id).page(params[:page])
   end

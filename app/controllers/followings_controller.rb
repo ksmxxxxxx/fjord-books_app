3a5ef24295b3d9954ef34d39ae4f0e1cbc4e19ea
ksmxxxxxx <kankichi69@gmail.com>
2022-07-29 18:11:49 +0900

    Show guidance when action has already been taken

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -2,12 +2,20 @@
 
 class FollowingsController < ApplicationController
   def create
-    current_user.follow(params[:id])
-    redirect_to user_path
+    if current_user.following?(User.find_by(params[:id]))
+      redirect_to user_path, alert: 'すでにフォローしています'
+    else
+      current_user.follow(params[:id])
+      redirect_to user_path
+    end
   end
 
   def destroy
-    current_user.unfollow(params[:id])
-    redirect_to user_path
+    if current_user.following?(User.find_by(params[:id]))
+      current_user.unfollow(params[:id])
+      redirect_to user_path
+    else
+      redirect_to user_path, alert: 'すでにフォロー解除しています'
+    end
   end
 end

56898c01f890b6f497c8812b0df7414760aac741
ksmxxxxxx <kankichi69@gmail.com>
2022-07-29 18:28:30 +0900

    Localize

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -2,8 +2,8 @@
 
 class FollowingsController < ApplicationController
   def create
-    if current_user.following?(User.find_by(params[:id]))
-      redirect_to user_path, alert: 'すでにフォローしています'
+    if current_user.following?(User.find_by!(params[:id]))
+      redirect_to user_path, notice: t('controllers.common.notice_already_following')
     else
       current_user.follow(params[:id])
       redirect_to user_path
@@ -11,11 +11,11 @@ class FollowingsController < ApplicationController
   end
 
   def destroy
-    if current_user.following?(User.find_by(params[:id]))
+    if current_user.following?(User.find_by!(params[:id]))
       current_user.unfollow(params[:id])
       redirect_to user_path
     else
-      redirect_to user_path, alert: 'すでにフォロー解除しています'
+      redirect_to user_path, notice: t('controllers.common.notice_already_unfollowed')
     end
   end
 end

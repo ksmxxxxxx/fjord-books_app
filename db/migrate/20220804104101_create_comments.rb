262dea81941f16618b0a6116638da785abcbd8f7
ksmxxxxxx <kankichi69@gmail.com>
2022-08-06 18:08:59 +0900

    WIP: commentモデルをbook,report,userと関連付け

--- db/migrate/20220804104101_create_comments.rb
+++ db/migrate/20220804104101_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.text :content
+      t.references :commentable, polymorphic: true, null: false
       t.references :user, foreign_key: true, null: false
-      t.references :report, foreign_key: true, null: false
 
       t.timestamps
     end

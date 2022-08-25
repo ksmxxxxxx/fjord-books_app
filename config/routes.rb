e8f5439411c54d82a418505c5a1b5bbc68bc8fa9
ksmxxxxxx <kankichi69@gmail.com>
2022-08-25 20:37:24 +0900

    Fix routes of comment to report

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :comments, only: %i[edit show update destroy]
   resources :reports do
-    resources :comments, module: :reports
+    resources :comments, module: :reports, only: %i[create edit update destroy]
   end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users

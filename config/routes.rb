9741a9431034c99d79dfe44644a8ed2356496364
ksmxxxxxx <kankichi69@gmail.com>
2022-08-25 20:42:11 +0900

    Tweak

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  resources :comments, only: %i[edit show update destroy]
-  resources :reports do
-    resources :comments, module: :reports, only: %i[create edit update destroy]
-  end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books do
-    resources :comments, module: :books
+    resources :comments, module: :books, only: %i[create edit update destroy]
+  end
+  resources :comments, only: %i[edit show update destroy]
+  resources :reports do
+    resources :comments, module: :reports, only: %i[create edit update destroy]
   end
   resources :users, only: %i[index show] do
     resource :relationships, only: %i[create destroy]

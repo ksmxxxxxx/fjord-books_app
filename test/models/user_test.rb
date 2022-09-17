d1d978a34cc438c40c3e89c731739c5ae04703dd
ksmxxxxxx <kankichi69@gmail.com>
2022-09-17 22:56:33 +0900

    Add test for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,43 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'login_with_email_or_name' do
+    user = User.new(email: 'foo@example.com', name: '')
+    assert_equal 'foo@example.com', user.name_or_email
+
+    user.name = 'bar'
+    assert_equal 'bar', user.name_or_email
+  end
+
+  test 'user_can_follow' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    they = User.create!(email: 'they@example.com', password: 'password')
+
+    assert_not me.following?(they)
+    me.follow(they)
+
+    assert me.following?(they)
+  end
+
+  test 'user_can_unfollow' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    they = User.create!(email: 'they@example.com', password: 'password')
+
+    assert_not me.following?(they)
+    me.follow(they)
+
+    assert me.following?(they)
+    me.unfollow(they)
+    assert_not me.following?(they)
+  end
+
+  test 'user_can_check_to_see_if_they_are_being_followed' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    they = User.create!(email: 'they@example.com', password: 'password')
+
+    assert_not me.followed_by?(they)
+    they.follow(me)
+
+    assert me.followed_by?(they)
+  end
 end

db597fdd95a13ec511713a8fb19e25b80056abf8
ksmxxxxxx <kankichi69@gmail.com>
2022-09-18 12:36:17 +0900

    Add test for report model

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,16 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:alice)
+    @report = reports(:day1)
+  end
+
+  test 'check_to_editable_user' do
+    assert @report.editable?(@user)
+  end
+
+  test 'convert_to_date_format' do
+    assert_equal reports(:day1).created_at.to_date, @report.created_on
+  end
 end

a085312d0418342357b46ce6fc00e8028bbfbf59
ksmxxxxxx <kankichi69@gmail.com>
2022-09-29 15:29:51 +0900

    Rename variable name and remove no need variable

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,38 +4,37 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   setup do
-    @user = users(:alice)
     @me = users(:alice)
-    @they = users(:bob)
+    @he = users(:bob)
   end
 
   test 'Login with email or name' do
-    assert_equal 'alice@example.com', @user.name_or_email
+    assert_equal 'alice@example.com', @me.name_or_email
 
-    @user.name = 'Alice'
-    assert_equal 'Alice', @user.name_or_email
+    @me.name = 'Alice'
+    assert_equal 'Alice', @me.name_or_email
   end
 
   test 'User can follow' do
-    assert_not @me.following?(@they)
-    @me.follow(@they)
+    assert_not @me.following?(@he)
+    @me.follow(@he)
 
-    assert @me.following?(@they)
+    assert @me.following?(@he)
   end
 
   test 'User can unfollow' do
-    assert_not @me.following?(@they)
-    @me.follow(@they)
+    assert_not @me.following?(@he)
+    @me.follow(@he)
 
-    assert @me.following?(@they)
-    @me.unfollow(@they)
-    assert_not @me.following?(@they)
+    assert @me.following?(@he)
+    @me.unfollow(@he)
+    assert_not @me.following?(@he)
   end
 
   test 'User can check to see if they are being followed' do
-    assert_not @me.followed_by?(@they)
-    @they.follow(@me)
+    assert_not @me.followed_by?(@he)
+    @he.follow(@me)
 
-    assert @me.followed_by?(@they)
+    assert @me.followed_by?(@he)
   end
 end
